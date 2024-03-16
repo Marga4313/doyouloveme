c9fe1cbf136887276026d7439bcaa3f1fda0c8a5
ed <115221238+Marga4313@users.noreply.github.com>
2024-03-16 16:29:57 +0800

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 let yes = document.getElementById("yes");
 let no = document.getElementById("no");
-// offsetTop:363
-// offsetLeft:180
 
+let count = 0;
 let px = "px";
 let X = Math.floor(Math.random() * 300);
 let Y = Math.floor(Math.random() * 250);
 no.addEventListener("click", () => {
   no.style.position = "absolute";
+  count++;
+  if (count === 10) alert("ede don't 🥺");
+  if (count === 15) alert("yah oh 🥹");
+  if (count === 20) alert("may gusto ka tlaga sakin tae mo 😎");
+  if (count === 25) alert("bakit baga hindi nalng mag yes papansin 😒");
+  if (count === 30) alert("dmoko matitiis tae mo 🙄");
+  if (count === 35)
+    alert("sumbong kita kalamo dimona pala ako love hohohohoho 😭");
+  if (count === 35) alert("dina kita love bahala ka 😤");
+  if (count === 50) alert("bat nandito kapa 😡");
+
   if (X <= 210) no.style.left = X + px;
   else no.style.left - X - 90 + px;
   if (Y <= 220) no.style.top = Y + px;
@@ -18,5 +28,7 @@ no.addEventListener("click", () => {
 });
 
 yes.addEventListener("click", () => {
-  alert("oh Thank you! and 'I Love You to' ❤️😘");
+  if (count <= 19) alert("oh Thank you! and 'I Love You too' ❤️😘");
+  else alert("sabi kona love mo tlaga ako pugi pugi ko eh 🥰");
+  count = 0;
 });
